7a29c875b008fe6fa866f05beeb01da807f75a18
unnati <unnati.sao@cbnits.com>
2026-01-28 14:47:51 +0530

    created a login page

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  
+}
