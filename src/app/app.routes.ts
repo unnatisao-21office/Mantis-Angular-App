15ada1e8eda355b5915651c4640421200f0ad95b
unnati <unnati.sao@cbnits.com>
2026-01-29 17:36:45 +0530

    added the login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { NewAccount } from './pages/new-account/new-account';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login
+    },{
+        path: 'newAccount',
+        component: NewAccount
+    }
+];
---
+++ src/app/pages/new-account/new-account.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-account',
+  imports: [],
+  templateUrl: './new-account.html',
+  styleUrl: './new-account.css',
+})
+export class NewAccount {
+
+}
