3f305dd1f277900ae2034b4aa40e8cc85e9d2824
unnati <unnati.sao@cbnits.com>
2026-02-04 14:53:29 +0530

    done with changes

--- src/app/pages/forget-password/forget-password.ts
+++ src/app/pages/forget-password/forget-password.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Router , RouterLink, RouterOutlet } from "@angular/router";
+import { Router  , RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-forget-password',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterLink],
   templateUrl: './forget-password.html',
   styleUrl: './forget-password.css',
   standalone: true,
